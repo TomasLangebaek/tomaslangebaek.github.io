e9dfd845538848d039762513adfdbeb6bf038979
TomasLangebaek <t.langebaek@uniandes.edu.co>
2023-03-22 16:31:56 -0500

    Update DescriptionCabanas.tsx

--- src/components/DescriptionCabanas.tsx
+++ src/components/DescriptionCabanas.tsx
@@ -21,8 +21,10 @@ export const DescriptionCabanas = () => {
                             <li><Typography variant="h6">20 m2</Typography></li>
                             <li><Typography variant="h6">Baño privado</Typography></li>
                             <li><Typography variant="h6">Capacidad para 2 personas</Typography></li>
+                            <li><Typography variant="h6">1 Cama queen</Typography></li>
                             <li><Typography variant="h6">Cocina totalmente equipada</Typography></li>
-                            <li><Typography variant="h6">Sala comedor</Typography></li>
+                            <li><Typography variant="h6">Deck Amplio</Typography></li>
+                            <li><Typography variant="h6">Una habitación</Typography></li>
                         </List>
                     </div>
                 </Grid>
